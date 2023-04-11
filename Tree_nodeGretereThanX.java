2c399d7477645bc781daeae27ab0abd8bd0ab24c
noobJonas <124491913+noobJonas@users.noreply.github.com>
2023-04-11 12:22:29 +0530

    Create Tree_nodeGretereThanX.java

---
+++ Tree_nodeGretereThanX.java
@@ -0,0 +1,60 @@
+import java.util.*;
+public class Solution {
+
+/*	TreeNode class 
+ * 
+ * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+
+	
+	public static int numNodeGreater(TreeNode<Integer> root,int x){
+
+		// Write your code here		
+		if(root == null) {
+			return 0;
+		}
+
+	
+		int count = 0;
+		
+		Queue<TreeNode<Integer>> q = new LinkedList<TreeNode<Integer>>();
+
+		q.add(root);
+		q.add(null);
+
+		while(!q.isEmpty()){
+			
+			TreeNode<Integer> temp = q.poll();
+			
+
+			if(temp == null){
+				
+				if(!q.isEmpty()){
+					q.add(null);
+				}
+			}
+			if(temp != null){
+				if(temp.data > x){
+					count++;
+				}
+				for(int i = 0 ; i<temp.children.size(); i++){
+					q.add(temp.children.get(i));
+				}
+			}
+
+		}
+
+
+		return count;
+		
+
+	}
+	
+}

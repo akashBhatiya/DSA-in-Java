9df3ddfa2fe5af757d698a3a4487434f5dc3932a
noobJonas <124491913+noobJonas@users.noreply.github.com>
2023-04-11 13:07:43 +0530

    Create Tree_height.java

---
+++ Tree_height.java
@@ -0,0 +1,58 @@
+import java.util.*;
+public class Solution {
+
+	/*	TreeNode structure 
+	 * 
+	 * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+
+	public static int getHeight(TreeNode<Integer> root){
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+		 * Taking input and printing output is handled automatically.
+		 */
+
+		if(root == null){
+			return 0;
+		}
+
+		int height = 0;
+		Queue<TreeNode<Integer>> q = new LinkedList<TreeNode<Integer>>();
+
+		q.add(root);
+		q.add(null);
+
+		while(!q.isEmpty()){
+			
+			TreeNode<Integer> temp = q.poll();
+			
+
+			if(temp == null){
+				
+				if(!q.isEmpty()){
+					q.add(null);
+				}
+				height++;
+			}
+			if(temp != null){
+				
+				for(int i = 0 ; i<temp.children.size(); i++){
+					q.add(temp.children.get(i));
+				}
+			}
+
+		}
+		return height;
+		
+	}
+
+}

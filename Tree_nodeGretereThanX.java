62399f3ffc0d38aa32c3c65aa2f76cbd980bb20e
noobJonas <124491913+noobJonas@users.noreply.github.com>
2023-04-11 15:38:04 +0530

    Tree with no. of nodes grater than x

--- Tree_nodeGretereThanX.java
+++ Tree_nodeGretereThanX.java
@@ -1,22 +1,8 @@
 import java.util.*;
 public class Solution {
 
-/*	TreeNode class 
- * 
- * class TreeNode<T> {
-		T data;
-		ArrayList<TreeNode<T>> children;
-
-		TreeNode(T data){
-			this.data = data;
-			children = new ArrayList<TreeNode<T>>();
-		}
-	}*/
-
-	
 	public static int numNodeGreater(TreeNode<Integer> root,int x){
-
-		// Write your code here		
+	
 		if(root == null) {
 			return 0;
 		}
